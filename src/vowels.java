8b1376a5f9829a544396945b9824eae9afa05fae
Aman VIshwakarma <amanvish7870@gmail.com>
2025-04-09 11:27:38 +0530

    commit

---
+++ src/vowels.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+
+public class vowels {
+    public static void main(String[] args) {
+        char s='a';
+
+        if(s=='a'|| s=='e'|| s=='i'|| s=='o'||s=='u'){
+            System.out.println("Its a Vowel");
+        }
+
+        else{
+            System.out.println("Its a Consonant");
+        }
+    }
+}
