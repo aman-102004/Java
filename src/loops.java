1c65be0f2a26ca5123737fcca3705071f549152e
Aman VIshwakarma <amanvish7870@gmail.com>
2025-04-11 11:13:06 +0530

    commit

---
+++ src/loops.java
@@ -0,0 +1,14 @@
+public class loops {
+    public static void main(String[] args) {
+        System.out.println("For loop");
+        for(int i=1;i<=5;i++){
+            System.out.println((i)+" Hello");
+        }
+        int a=1;
+        System.out.println("While loop");
+        while(a<5){
+            System.out.println((a)+" Hello");
+            a++;
+        }
+    }
+}
