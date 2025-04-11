3bf417ff7f07006114f6e119c180b522787207fb
Aman VIshwakarma <amanvish7870@gmail.com>
2025-04-11 10:51:22 +0530

    commit

--- src/userinput.java
+++ src/userinput.java
@@ -6,21 +6,25 @@ public class userinput {
         Scanner in= new Scanner(System.in);
 
         System.out.println("Enter the Value of A");
-        int a= in.nextInt();
+        float a= in.nextInt();
         System.out.println("Enter the Value of B");
-        int b=in.nextInt();
+        float b=in.nextInt();
         System.out.println("Enter the Operator(+,-,*,/)");
         char c=in.next().charAt(0);
 
         switch (c){
             case '+':
                 System.out.println("Additon of "+ a + " & "+ b +" is "+(a+b) );
+                break;
             case '-':
                 System.out.println("Subtraction of "+ a + " & "+ b +" is "+(a-b));
+                break;
             case '*':
                 System.out.println("Multiplication of "+ a + " & "+ b +" is "+(a*b) );
+                break;
             case '/':
                 System.out.println("Division of "+ a + " & "+ b +" is "+(float)(a/b) );
+                break;
         }
 
 
