10959d1196823f57b89f6d3b526f6c7efdad9365
Aman VIshwakarma <amanvish7870@gmail.com>
2025-04-11 10:11:18 +0530

    commit

---
+++ src/switch1.java
@@ -0,0 +1,16 @@
+public class switch1 {
+    public static void main(String[] args) {
+        int a=1;
+        switch(a){
+            case 1:
+                System.out.println("Byeeee");
+                break;
+
+            case 2:
+                System.out.println("Two");
+                break;
+            default:
+                System.out.println("Hiiiii");
+        }
+    }
+}
